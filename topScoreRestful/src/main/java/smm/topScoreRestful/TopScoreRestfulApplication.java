972504b081f84d469e20265c9b7b142703636efb
sisminnmaw <sisminnmaw@gmail.com>
2020-11-29 10:55:39 +0900

    first commit
    hello testing console

---
+++ topScoreRestful/src/main/java/smm/topScoreRestful/TopScoreRestfulApplication.java
@@ -0,0 +1,14 @@
+package smm.topScoreRestful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopScoreRestfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopScoreRestfulApplication.class, args);
+		System.out.println("hello testing");
+	}
+
+}
---
+++ topScoreRestful/src/test/java/smm/topScoreRestful/TopScoreRestfulApplicationTests.java
@@ -0,0 +1,13 @@
+package smm.topScoreRestful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopScoreRestfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
